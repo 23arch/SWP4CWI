5a3d384cb776aed6dc9bfae21659a30516321579
unknown <talha.yuecel@student.htldornbirn.at>
2023-11-28 10:01:07 +0100

    API

--- script.js
+++ script.js
@@ -1,47 +1,32 @@
-const loadData = () => {
-    fetch("").then((result) =>{
-        result.json().then((data) => {
-            console.log(data);
-            filltable(data);
-        });
-    });
-};
- 
-const filltable =  (data) => {
-    let html = "";
-    data.array.forEach(element => {
-        html += "<div>" + element.points + " by Team" +  element.teamName + "</div>";    
-    });
-    document.getElementById("").innerHTML = html;
-};  
- 
-localData();
+document.addEventListener("DOMContentLoaded", function () {
+    const apiUrl = "https://api.openligadb.de/getbltable/öbl1/2023";
 
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log("data:", data);
+            
+            // Sortiere die Teams basierend auf der Platzierung
+            const sortedData = data.sort((a, b) => a.place - b.place);
 
+            const apiDataContainer = document.getElementById("result");
 
-function loadMatches(){
- 
-    fetch('https://api-basketball.p.rapidapi.com/')
-      .then(response => response.json())
-      .then(json => fillTable(json))
-}
- 
- 
-function fillTable(data){
-    console.log(data);
-    let table = "";
-    data.forEach((team, index) => {
-        // let position = index +1;
-       let output =
-       
-       '<div class="component">' + team.teamName +
-       '<div class="wappen"><img src="' + team.teamIconUrl + '" alt="" height="35px" width="35px">' +
-       '</div><div class="pk">' + team.points + '</div> <div class="Games">' + team.matches + '</div> <div class="Sieg">' + team.won + '</div>' +
-       '<div class="Unent">' + team.draw + '</div> <div class="Nied">' + team.lost + '</div> </div>';
- 
-       table += output;
-    });
-    document.getElementById("table").innerHTML=table;
-}
- 
-loadMatches();
+            sortedData.forEach(team => {
+                console.log(team.teamName);
+                const teamElement = document.createElement("div");
+                teamElement.classList.add("wrapper");
+
+                teamElement.innerHTML = `
+                    <img class="logo" src="${team.teamIconUrl}" style="height: 40px;">
+                    <div class="team">${team.teamName}</div>
+                    <div class="circle">${team.won}</div>
+                    <div class="circle">${team.lost}</div>
+                    <div class="circle">${team.points}</div>
+                    <div class="wrt">${team.goalDiff}</div>
+                `;
+
+                apiDataContainer.appendChild(teamElement);
+            });
+        })
+        .catch(error => console.error("Error fetching data:", error));
+});
